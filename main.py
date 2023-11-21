1973ef893295c5e05923b4506a3701cb1b7c058b
pedrohcs8 <pedrohenriquecsena@gmail.com>
2023-11-20 23:16:53 -0400

    v1.1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import time
-import re
 from simple_term_menu import TerminalMenu
+
+import time
 import atexit
 import os
+import re
 from operator import itemgetter
 
 url_ml       = "https://lista.mercadolivre.com.br/"
@@ -20,7 +21,7 @@ list_pichau = list()
 list_amazon = list()
 
 # Acha os produtos no Mercado Livre
-def produtosMl():
+def produtosMl(useStrictTerm, strictTerm):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -45,11 +46,12 @@ def produtosMl():
 
         tituloProduto = info_produto.find('h2', 'ui-search-item__title').text
 
-        if nomeProduto not in tituloProduto:
-            continue
+        if useStrictTerm:
+            if strictTerm not in tituloProduto:
+                    continue
 
         linkProduto = info_produto.find('a', 'ui-search-item__group__element ui-search-link')['href']
-        precoProduto = info_produto.find('span', 'andes-money-amount__fraction').text
+        precoProduto = int(info_produto.find('span', 'andes-money-amount__fraction').text.replace('.', ''))
 
         list_ml.append(list([tituloProduto, precoProduto, linkProduto]))
 
@@ -58,7 +60,7 @@ def produtosMl():
     return sorted_list[:quantidadeProdutos]
 
 # Acha os produtos na Kabum
-def produtosKabum():
+def produtosKabum(useStrictTerm, strictTerm):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -81,11 +83,15 @@ def produtosKabum():
     for produto in produtos_kabum:
         tituloProduto = produto.find('span', 'sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard').text
 
-        if nomeProduto not in tituloProduto:
-            continue
+        if useStrictTerm:
+            if strictTerm not in tituloProduto:
+                    continue
 
         linkProduto = produto.find('a', 'sc-cdc9b13f-10 jaPdUR productLink')['href']
-        precoProduto = produto.find('span', 'sc-620f2d27-2 bMHwXA priceCard').text
+        precoProdutoBruto = produto.find('span', 'sc-620f2d27-2 bMHwXA priceCard').text
+        precoProdutoSelecionado = re.findall(r'\d+', precoProdutoBruto)
+
+        precoProduto = int(''.join(precoProdutoSelecionado[:2]))
 
         list_kabum.append(list([tituloProduto, precoProduto, linkProduto]))
 
@@ -93,7 +99,7 @@ def produtosKabum():
 
     return sorted_list[:quantidadeProdutos]
 
-def produtosPichau():
+def produtosPichau(useStrictTerm, strictTerm):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -109,22 +115,28 @@ def produtosPichau():
     site_pichau = BeautifulSoup(page, 'html.parser')
     produtos_pichau = site_pichau.find_all('div', attrs={'class': 'MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-md-4 MuiGrid-grid-lg-3 MuiGrid-grid-xl-2'})
 
+    i = 0
     for produto in produtos_pichau:
-        tituloProduto = produto.find('h2', 'MuiTypography-root jss80 jss81 MuiTypography-h6').text
+        tituloProduto = produto.find('h2', 'MuiTypography-root jss232 jss233 MuiTypography-h6').text
 
-        if nomeProduto not in tituloProduto:
-            continue
+        if useStrictTerm:
+            if strictTerm not in tituloProduto:
+                    continue
 
-        linkProduto = 'https://pichau.com.br/' + produto.find('a', 'jss16')['href']
-        precoProduto = produto.find('div', 'jss83').text
+        linkProduto = 'https://pichau.com.br/' + produto.find('a', 'jss166')['href']
+        precoProdutoBruto = produto.find('div', 'jss235').text
+        precoProdutoSelecionado = re.findall(r'\d+', precoProdutoBruto)
+
+        precoProduto = int(''.join(precoProdutoSelecionado[:2]))
 
         list_pichau.append(list([tituloProduto, precoProduto, linkProduto]))
+        i = i + 1
 
     sorted_list = sorted(list_pichau, key=itemgetter(1))
 
     return sorted_list[:quantidadeProdutos]
 
-def produtosAmazon():
+def produtosAmazon(useStrictTerm, strictTerm):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -143,29 +155,26 @@ def produtosAmazon():
     for produto in produtos_amazon:
         tituloProduto = produto.find('span', 'a-size-base-plus a-color-base a-text-normal').text
 
-        if nomeProduto not in tituloProduto:
-            continue
+        if useStrictTerm:
+            if strictTerm not in tituloProduto:
+                continue
 
         linkProduto = 'https://amazon.com.br' + produto.find('a', 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')['href']
 
-        precoProduto = ''
-
         if (produto.find('span', 'a-size-base a-color-secondary') is None or produto.find('span', 'a-size-base a-color-secondary').text == 'Nenhuma opção de compra em destaque'):
-            precoProduto = 'Indisponivel'
+            continue
 
-        if (precoProduto != 'Indisponivel'):
-            precoInteiro = produto.find('span', 'a-price-whole').text
-            precoCentavos = produto.find('span', 'a-price-fraction').text
-            precoProduto = precoInteiro + precoCentavos
+        precoInteiro = produto.find('span', 'a-price-whole').text
+        precoProduto = precoInteiro.replace('.', '').replace(',', '')
 
-        list_amazon.append(list([tituloProduto, precoProduto, linkProduto]))
+        list_amazon.append(list([tituloProduto, int(precoProduto), linkProduto]))
 
     sorted_list = sorted(list_amazon, key=itemgetter(1))
 
     return sorted_list[:quantidadeProdutos]
 
 options = ['Mercado Livre', 'Amazon', 'Kabum', 'Pichau']
-terminal_menu = TerminalMenu(options, multi_select=True, show_multi_select_hint=True, multi_select_empty_ok=True, multi_select_select_on_accept=False)
+terminal_menu = TerminalMenu(options, multi_select=True, show_multi_select_hint=True, multi_select_empty_ok=True, multi_select_select_on_accept=False, title="Selecione a(as) lojas")
 menu_entry_indices = terminal_menu.show()
 selectedStores = terminal_menu.chosen_menu_entries
 
@@ -174,14 +183,31 @@ if selectedStores is None or len(selectedStores) < 1:
     exit(1)
 
 nomeProduto = input('Qual o produto desejado? ')
-quantidadeProdutos = int(input('Quantos produtos por loja devo exibir? '))
+
+quantidadeProdutos = 0
+quantidadeProdutosBruto = input('Quantos produtos por loja devo exibir? ')
+if quantidadeProdutosBruto.isnumeric():
+    quantidadeProdutos = int(quantidadeProdutosBruto)
+else:
+    print('Você deve colocar um número!')
+    exit(1)
+
+strict_menu = TerminalMenu(['Não', 'Sim'], title='Usar palavra chave?')
+menu_entry_index = strict_menu.show()
+
+useStrictTerm = False
+strictTerm = ''
+
+if menu_entry_index == 1:
+    strictTerm = input("Qual será a palavra chave? ")
+    useStrictTerm = True
 
 if nomeProduto == '':
     print('Selecione um produto!')
     exit(1)
 
-if int(quantidadeProdutos) > 5:
-    print('Só posso exibir até 5 produtos!')
+if int(quantidadeProdutos) > 10:
+    print('Só posso exibir até 10 produtos!')
     exit(1)
 elif int(quantidadeProdutos) < 1:
     print('A quantidade de produtos deve ser maior que 1!')
@@ -193,16 +219,16 @@ for store in selectedStores:
     match store:
         case 'Mercado Livre':
             productList.append('Mercado Livre')
-            productList.append(produtosMl())
+            productList.append(produtosMl(useStrictTerm, strictTerm))
         case 'Amazon':
             productList.append('Amazon')
-            productList.append(produtosAmazon())
+            productList.append(produtosAmazon(useStrictTerm, strictTerm))
         case 'Kabum':
             productList.append('Kabum')
-            productList.append(produtosKabum())
+            productList.append(produtosKabum(useStrictTerm, strictTerm))
         case 'Pichau':
             productList.append('Pichau')
-            productList.append(produtosPichau())
+            productList.append(produtosPichau(useStrictTerm, strictTerm))
 
 def final():
     if os.path.isfile('produtos.txt'):
@@ -221,7 +247,7 @@ def final():
             for product in products:
                 file.write(product[0])
                 file.write('\n')
-                file.write(product[1])
+                file.write(str(product[1]))
                 file.write('\n')
                 file.write(product[2])
                 file.write('\n')
